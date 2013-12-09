21ef9efc81af77a13ee4e0cbaab4db83e7c22269
Steven Miranda <miranda@etu.unistra.fr>
2013-12-09 22:04:51 +0100

    Transformed defines in quad.h in an enum

--- quad.h
+++ quad.h
@@ -1,30 +1,6 @@
 #ifndef __QUAD__
 #define __QUAD__
 
-#define AFFEC 						1
-#define AFFEC_UNARY_MINUS 			2
-#define AFFEC_UNARY_NOT 			3
-#define AFFEC_BINARY_PLUS			4
-#define AFFEC_BINARY_MINUS			5
-#define AFFEC_BINARY_MULT			6
-#define AFFEC_BINARY_DIV			7
-#define AFFEC_BINARY_OR				8
-#define AFFEC_BINARY_AND			9
-
-#define BRANCHMENT_UNCOND			20
-#define BRANCHMENT_COND_EQ			21
-#define BRANCHMENT_COND_NEQ			22
-#define BRANCHMENT_COND_LT			23
-#define BRANCHMENT_COND_LTEQ		24
-#define BRANCHMENT_COND_GT			25
-#define BRANCHMENT_COND_GTEQ		26
-
-#define PROC_PARAM					30
-#define PROC_CALL					31
-
-#define TAB_LEFT					40
-#define TAB_RIGHT					41
-
 	typedef struct quad {
 		char* label;
 		struct symbol* operande1;
